9a14f6fb4b52bc0554a58a241c62f99ef5d19301
Shourya Sharma <shouryano01@gmail.com>
2026-06-18 20:10:30 +0530

    Added more questions

--- Leetcode/DAY 17/stackusingqueue.java
+++ Leetcode/DAY 17/stackusingqueue.java
@@ -1,3 +1,51 @@
 class stackusingqueue{
+    class MyStack {
+
+    Queue<Integer> q;
+    Queue<Integer> temp;
+    public MyStack() {
+        q = new LinkedList<>();
+        temp= new LinkedList<>();
+    }
     
+    public void push(int x) {
+        q.add(x);
+    }
+    
+    public int pop() {
+        while (q.size() > 1) {
+            temp.add(q.remove());
+        }
+        int val = q.remove();
+        while (!temp.isEmpty()) {
+            q.add(temp.remove());
+        }
+        return val;
+    }
+    
+    public int top() {
+        int val = 0;
+        while(!q.isEmpty()){
+            val = q.peek();
+            temp.add(q.remove());
+        }
+        while(!temp.isEmpty()){
+            q.add(temp.remove());
+        }
+        return val;
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
 }
