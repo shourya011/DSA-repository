fb59369d74be7b4d93ca6065f3f53d1c6b851a76
Shourya Sharma <shouryano01@gmail.com>
2026-07-01 18:24:27 +0530

    Added alternative of binarytreepath

--- Tree/binarytreepath.java
+++ Tree/binarytreepath.java
@@ -36,3 +36,33 @@ class binarytreepath {
         helper(root.right,path,list);
     }
 }
+
+
+
+//gfg
+class Solution {
+    public ArrayList<ArrayList<Integer>> Paths(Node root) {
+        // code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> sublist = new ArrayList<>();
+        if(root==null) return list;
+        helper(root,list,sublist);
+        return list;
+    }
+    public void helper(Node root,ArrayList<ArrayList<Integer>> list,ArrayList<Integer> sublist){
+        if(root==null) return;
+        sublist.add(root.data);
+        if(root.left==null && root.right==null){
+            list.add(new ArrayList<>(sublist));
+            sublist.remove(sublist.size()-1); //backtracking
+            return;
+        }
+        helper(root.left,list,sublist);
+        helper(root.right,list,sublist);
+        sublist.remove(sublist.size()-1); //backtracking
+    }
+}
+
+
+// list.add(sublist) → saves the original notebook.
+// list.add(new ArrayList<>(sublist)) → saves a photocopy of the current page.
