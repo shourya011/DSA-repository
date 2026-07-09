f2c05733d7c75e31bcc481a77fc6193352c280bc
Shourya Sharma <shouryano01@gmail.com>
2026-07-09 20:40:33 +0530

    DAY 38

---
+++ Algorithm/Binary Seach/Mininrotated.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0,h = nums.length-1;
+        int mid = 0;
+        int min = 0;
+        while(l<h){
+            mid = l + (h-l)/2;
+            if(nums[mid]>nums[h]){
+                l = mid + 1;
+            }
+            else if(nums[mid]<nums[h]){
+                h = mid;
+            }
+        }
+        return nums[l];
+    }
+}
