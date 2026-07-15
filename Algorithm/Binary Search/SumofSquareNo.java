da059db5f5e86cf685a61b34836aee30c97f8b39
Shourya Sharma <shouryano01@gmail.com>
2026-07-15 20:12:42 +0530

    Changed directory name and added one question

---
+++ Algorithm/Binary Search/SumofSquareNo.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long low = 0;
+        long high = (long)Math.sqrt(c);
+        while(low<=high){
+            long sum = (long) low*low + high*high;
+            if(sum==c){
+                return true;
+            }
+            else if(sum<c){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return false;
+    }
+}
