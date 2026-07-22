6e3baaf43f055c40bdd0eeb4006d079ce83ea664
Shourya Sharma <shouryano01@gmail.com>
2026-07-22 20:18:57 +0530

    Added one more approach

--- String/RotateString.java
+++ String/RotateString.java
@@ -15,3 +15,13 @@ class Solution {
         return false;
     }
 }
+
+
+// if(s.length()!=goal.length()){
+//             return false;
+//         }
+//         String temp = s + s;
+//         if(temp.contains(goal)){
+//             return true;
+//         }
+//         return false;
