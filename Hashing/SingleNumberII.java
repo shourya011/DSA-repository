d4e0e3758d2ca834a3807ee2347c18c4b9884310
Shourya Sharma <shouryano01@gmail.com>
2026-08-13 18:29:46 +0530

    DAY 66 : 137. Single Number II

---
+++ Hashing/SingleNumberII.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else
+                map.put(nums[i],1);
+        }
+        for(Map.Entry<Integer,Integer> e : map.entrySet()){
+            if(e.getValue()==1){
+                return e.getKey();
+            }
+        }
+        return -1;
+    }
+}
+
+//137. Single Number II
